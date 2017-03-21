f5182378d19982b850d3a49d271ee51e8272869e
Mosaic <wcmbulldog@gmail.com>
2017-03-21 10:04:16 -0500

    get your shit together, lucas

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def on_message(message):
             await client.send_message(message.channel, 'quack')
             
         elif command == "angery"
-           await client.send_message(message,channel, ':angry:')
+            await client.send_message(message.channel, ':angry:')
             
         elif command == "sipp":
             await client.send_message(message.channel, 'why sipp when you can s u c c')
@@ -53,5 +53,11 @@ async def on_message(message):
             
         elif command == "succ":
             await client.send_message(message.channel, 'l...lewd')
+            
+        elif message.content.startswith('sup, nico'):
+            await client.send_message(message.channel, 'YO YO YO YO! WHAT IT IS, MOTHERFUCKERS!?')
+                                      
+        elif message.content.startswith('nico nico ni'):
+            await client.send_message(message.channel, 'Stop')
 
 client.run('no')
